6aa0ab1d90a2ba04cc09bd8012c8212f3f17203d
Ashwinisevankar <68225125+Ashwinisevankar@users.noreply.github.com>
2020-07-17 14:15:07 +0530

    Assignment Day 3

---
+++ Assignment Day 3/script/evenodd.js
@@ -0,0 +1,14 @@
+//Even or Odd
+let x =+prompt("Enter Number",0);
+{
+    if (x === 0) {
+            console.log( "The number entered is " + x +  " and Number is even");
+    }
+    else if (x % 2 === 0) {
+            console.log("The number entered is " + x +  " and Number is even");   
+    }
+    else {
+            console.log("The number entered is " + x +  " and Number is odd");
+    }
+}
+
---
+++ Assignment Day 3/script/version.js
@@ -0,0 +1,5 @@
+// OS Name form user
+let x =prompt("Enter OS name: ");
+let y =+prompt("Enter OS version",0);
+console.log("The OS Name is "+'\xa0' + x + " and version is " + y);
+ 
